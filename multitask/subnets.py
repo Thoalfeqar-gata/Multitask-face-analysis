f294b462f38739c69f666de4047c5c59436ae6a3
Thoalfeqar-gata <thoalfeqarkatajarullah@gmail.com>
2025-11-15 13:42:42 +0300

    Chaned the name and the way the face recognition subnet works by moving the margin head out of the subnet

--- multitask/subnets.py
+++ multitask/subnets.py
@@ -8,20 +8,14 @@ from multitask.fusion import MultiScaleFusion
 from multitask.cbam import CBAM
 
 
-class FaceRecognitionSubnet(nn.Module):
+class FaceRecognitionEmbeddingSubnet(nn.Module):
     def __init__(
             self,
-            classnum,
-            head_type = 'adaface',
             feature_embedding_dim = 768, 
             embedding_dim = 512,
-            m = 0.4, 
-            t_alpha = 1.0, 
-            h = 0.333, 
-            s = 64.0,
         ):
         
-        super(FaceRecognitionSubnet, self).__init__()
+        super(FaceRecognitionEmbeddingSubnet, self).__init__()
 
         self.norm_layer = nn.LayerNorm(feature_embedding_dim)
         self.avgpool = nn.AdaptiveAvgPool1d(1)
@@ -32,35 +26,24 @@ class FaceRecognitionSubnet(nn.Module):
             nn.BatchNorm1d(num_features = embedding_dim, eps = 2e-5)
         )
 
-        self.head = build_head(
-            head_type = head_type,
-            embedding_size = embedding_dim,
-            classnum = classnum,
-            m = m,
-            t_alpha = t_alpha,
-            h = h,
-            s = s,
-        )
             
-    def forward(self, multiscale_features, labels):
-        x = multiscale_features[-1] #obtain the feature vector from the last stage, which has a shape of 49x768
+    def forward(self, multiscale_features):
+        x = multiscale_features[-1] #obtain the feature vector from the last stage, which has a shape of 49x768 or 49x1024
         x = self.norm_layer(x) # B, 49, 768 
         x = self.avgpool(x.transpose(1, 2)) # B, 768, 1
         x = torch.flatten(x, 1) # B, 768
         embedding = self.feature_head(x)
         embedding_norm = torch.norm(embedding, p=2, dim=1, keepdim = True)
         normalized_embedding = torch.div(embedding, embedding_norm)
-        
-        logits = self.head(normalized_embedding, embedding_norm, labels)
 
-        return logits
+        return normalized_embedding, embedding_norm
 
 
 class AgeEstimationSubnet(nn.Module):
-    def __init__(self):
+    def __init__(self, transformer_embedding_dim = 96):
         super(AgeEstimationSubnet, self).__init__()
 
-        self.fusion = MultiScaleFusion(out_channels=[47, 93, 186, 186])
+        self.fusion = MultiScaleFusion(out_channels=[47, 93, 186, 186], transformer_embedding_dim=transformer_embedding_dim)
         self.cbam = CBAM(channels=512)
         self.head = nn.Sequential( # A simple regression head
             nn.AdaptiveMaxPool2d((1, 1)), # 7x7x512 ->1x1x512
@@ -78,10 +61,10 @@ class AgeEstimationSubnet(nn.Module):
 
 
 class GenderEstimationSubnet(nn.Module):
-    def __init__(self):
+    def __init__(self, transformer_embedding_dim = 96):
         super(GenderEstimationSubnet, self).__init__()
         
-        self.fusion = MultiScaleFusion(out_channels=[47, 93, 186, 186])
+        self.fusion = MultiScaleFusion(out_channels=[47, 93, 186, 186], transformer_embedding_dim=96)
         self.cbam = CBAM(channels=512)
         self.head = nn.Sequential( # A simple binary classification head
             nn.AdaptiveMaxPool2d((1, 1)), # 7x7x512 -> 1x1x512
@@ -100,9 +83,11 @@ class GenderEstimationSubnet(nn.Module):
 
 
 class EmotionRecognitionSubnet(nn.Module):
-    def __init__(self, num_classes = 7):
+    def __init__(self, num_classes = 7, transformer_embedding_dim = 96):
         super(EmotionRecognitionSubnet, self).__init__()
 
+        self.fusion = MultiScaleFusion(out_channels=[47, 93, 186, 186], transformer_embedding_dim=transformer_embedding_dim)
+        self.cbam = CBAM(channels=512)
         self.head = nn.Sequential(
             nn.AdaptiveMaxPool2d((1, 1)), # 7x7x512 -> 1x1x512
             nn.Flatten(), # 1x1x512 -> 512
@@ -110,9 +95,9 @@ class EmotionRecognitionSubnet(nn.Module):
             nn.Linear(in_features = 512, out_features = 256, bias = True),
             nn.ReLU(),
             nn.Linear(in_features = 256, out_features = num_classes, bias = True),
-
         )
 
-
-    def forward(self, x): # x represents the fused features
+    def forward(self, multiscale_features):
+        x = self.fusion(multiscale_features)
+        x = self.cbam(x)
         return self.head(x)
