35073dd67b6d43166466316ce80eb4a828efa09d
Thoalfeqar-gata <thoalfeqarkatajarullah@gmail.com>
2026-01-06 22:50:08 +0300

    Finished preparing BIWI

--- utility_scripts/pose_estimation_utilities.py
+++ utility_scripts/pose_estimation_utilities.py
@@ -140,3 +140,49 @@ def matrix_to_euler_angles(matrix, to_degrees=True):
         euler_angles = torch.rad2deg(euler_angles)
         
     return euler_angles
+
+
+import cv2
+def draw_axis(img, yaw, pitch, roll, tdx=None, tdy=None, size=50, degrees = True):
+    """
+    img: numpy array (H, W, 3) or (H, W) - Input image
+    yaw, pitch, roll: angles in degrees
+    tdx, tdy: Center of the face (x, y). If None, uses image center.
+    size: Length of the axis lines
+    """
+    # Convert degrees to radians
+    if degrees:
+        pitch = pitch * np.pi / 180
+        yaw = -(yaw * np.pi / 180) 
+        roll = roll * np.pi / 180
+
+    if tdx is None or tdy is None:
+        height, width = img.shape[:2]
+        tdx = width / 2
+        tdy = height / 2
+
+    # X-Axis pointing to right. drawn in Red
+    x1 = size * (np.cos(yaw) * np.cos(roll)) + tdx
+    y1 = size * (np.cos(pitch) * np.sin(roll) + np.cos(roll) * np.sin(pitch) * np.sin(yaw)) + tdy
+
+    # Y-Axis | drawn in Green
+    #        v
+    x2 = size * (-np.cos(yaw) * np.sin(roll)) + tdx
+    y2 = size * (np.cos(pitch) * np.cos(roll) - np.sin(pitch) * np.sin(yaw) * np.sin(roll)) + tdy
+
+    # Z-Axis (out of screen) drawn in Blue
+    x3 = size * (np.sin(yaw)) + tdx
+    y3 = size * (-np.cos(yaw) * np.sin(pitch)) + tdy
+
+    # Draw the lines
+    # OpenCV uses BGR, so (0,0,255) is Red, (0,255,0) is Green
+    img = img.copy()
+    
+    # Pitch (X-axis) - Red
+    cv2.line(img, (int(tdx), int(tdy)), (int(x1),int(y1)),(255, 0, 0),2)
+    # Yaw (Y-axis) - Green
+    cv2.line(img, (int(tdx), int(tdy)), (int(x2),int(y2)),(0,255,0),2)
+    # Roll (Z-axis) - Blue
+    cv2.line(img, (int(tdx), int(tdy)), (int(x3),int(y3)),(0, 0, 255),2)
+
+    return img
